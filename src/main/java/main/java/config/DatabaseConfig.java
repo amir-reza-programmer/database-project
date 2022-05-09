3fc8b7812ed500343a3b69437d3536ec3e305dab
amir-reza-programmer <amirrezazare130081@gmail.com>
2022-05-09 19:37:18 +0430

    fill package directories

---
+++ src/main/java/main/java/config/DatabaseConfig.java
@@ -0,0 +1,14 @@
+package main.java.config;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author amirreza
+ */
+public class DatabaseConfig {
+    
+}
